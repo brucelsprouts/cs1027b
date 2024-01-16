aa49ed1c649e5dd49931e10b3fc286161b755164
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2024-01-16 15:05:54 -0500

    asd

---
+++ labs/lab 1/Player.java
@@ -0,0 +1,37 @@
+import package.Player;
+
+public class Player {
+    private String name;
+    private String position;
+    private int jerseyNum;
+
+    public Player(String name, String position, int jerseyNum){
+        name = this.name;
+        position = this.position;
+        jerseyNum = this.jerseyNum;
+    }
+
+    public String getName(){
+
+    }
+
+    public String getPosition(){
+        
+    }
+
+    public int getJerseyNum(){
+        
+    }
+
+    public void setName(String newName){
+
+    }
+
+    public void setPosition(String newName){
+
+    }
+
+    public void setJerseyNum(int newName){
+
+    }
+}
---
+++ labs/lab 1/TestLab.java
@@ -0,0 +1,10 @@
+import package.Player;
+
+public class TestLab {
+    public static void main(String args[]){
+        Player p1 = new Player("Bruce","goalie",72);
+        System.out.println(p1.getName());
+        System.out.println(p1.getPosition());
+        System.out.println(p1.getJerseyNum());
+    }
+}
