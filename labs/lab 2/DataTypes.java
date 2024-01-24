1d9493c3c9c899f2e6bf205da8142067648edf9a
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2024-01-23 21:58:57 -0500

    awd

---
+++ labs/lab 2/DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+    public int intVar;
+    public double doubleVar;
+    public char charVar;
+    public boolean boolVar;
+    public ClassA varA;
+
+    public DataTypes() {
+    }
+
+    public DataTypes(int newIntVar, double newDoubleVar, char newCharVar, boolean newBoolVar, ClassA newVarA) {
+        intVar = newIntVar;
+        doubleVar = newDoubleVar;
+        charVar = newCharVar;
+        boolVar = newBoolVar;
+        varA = newVarA;
+    }
+
+    public boolean equals(DataTypes other) {
+        if (this.intVar != other.intVar || this.doubleVar != other.doubleVar ||
+            this.charVar != other.charVar || this.boolVar != other.boolVar) {
+            return false;
+        }
+
+        return this.varA.equals(other.varA);
+    }
+}
